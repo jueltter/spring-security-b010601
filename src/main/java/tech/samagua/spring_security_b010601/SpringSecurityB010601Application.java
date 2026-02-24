6237f906a99b782c8897b659d93025880cfd2e91
Stalin Amagua <jueltter@users.noreply.github.com>
2026-02-23 22:35:09 -0500

    initial commit

---
+++ src/main/java/tech/samagua/spring_security_b010601/SpringSecurityB010601Application.java
@@ -0,0 +1,13 @@
+package tech.samagua.spring_security_b010601;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityB010601Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityB010601Application.class, args);
+	}
+
+}
---
+++ src/test/java/tech/samagua/spring_security_b010601/SpringSecurityB010601ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.samagua.spring_security_b010601;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityB010601ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
